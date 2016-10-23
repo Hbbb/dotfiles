b14772d9be7e2915ede1aa67fdf55b6a195a76af
Harrison Borges <hbb@Harrisons-MacBook-Pro.local>
2016-10-22 19:29:44 -0700

    basic dotfiles config

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Symlinking dotfiles to home directory"
+for file in `ls files`
+do
+  if [ -e ~/.$file -o -L ~/.$file ]; then
+    echo "Replacing link to ~/$file"
+    rm -rf ~/.$file
+  fi
+  ln -s $PWD/files/$file ~/.$file
+done
