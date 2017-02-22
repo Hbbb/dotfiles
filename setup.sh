2326ac27728142877113f98b0741df7ab496f61a
Harrison <hborges005@gmail.com>
2017-02-22 18:16:03 +0000

    Setup should clone submobules first, then symlink

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Cloning submodules"
+git submodule update --init --recursive
+
 echo "Symlinking dotfiles to home directory"
 for file in `ls files`
 do
@@ -11,5 +14,3 @@ do
   ln -s $PWD/files/$file ~/.$file
 done
 
-echo "Cloning submodules"
-git submodule update --init --recursive
